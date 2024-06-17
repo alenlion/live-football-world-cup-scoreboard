1ac45cfabe5fa84642249ce31d770d1b14985d97
Rayan <Rayan.Aksu@gmail.com>
2024-06-17 15:31:46 +0300

    create Team class

---
+++ src/main/java/org/rayan/scorboard/Team.java
@@ -0,0 +1,28 @@
+package org.rayan.scorboard;
+
+/**
+ * @author Rayan Aksu
+ * @since 6/17/2024
+ */
+
+public class Team {
+    private final String name;
+
+    public Team( String name ) {
+        this.name = name;
+        this.score = 0;
+    }
+    public String getName() {
+        return name;
+    }
+
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore( int score ) {
+        this.score = score;
+    }
+}

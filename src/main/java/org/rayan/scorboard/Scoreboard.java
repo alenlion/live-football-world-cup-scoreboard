b934d6e3fb86746c597136aa17065814ff9c653e
Rayan <Rayan.Aksu@gmail.com>
2024-06-17 15:21:44 +0300

    write startMatch to pass test

--- src/main/java/org/rayan/scorboard/Scoreboard.java
+++ src/main/java/org/rayan/scorboard/Scoreboard.java
@@ -1,9 +1,23 @@
 package org.rayan.scorboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Rayan Aksu
  * @since 6/17/2024
  */
 
 public class Scoreboard {
+    private List<Match> matches;
+
+    public Scoreboard() {
+        this.matches = new ArrayList<>();
+    }
+    public void startNewMatch(String homeTeam, String awayTeam) {
+        matches.add(new Match(homeTeam, awayTeam));
+    }
+    public List<Match> getMatches() {
+        return matches;
+    }
 }

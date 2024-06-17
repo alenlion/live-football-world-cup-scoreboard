78c598e769ef124821c7f44ef5a79ea4cc05a0d7
Rayan <Rayan.Aksu@gmail.com>
2024-06-17 15:38:39 +0300

    refactor start match test to check the init score = 0

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -1,5 +1,7 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.rayan.scorboard.Scoreboard;
+import org.rayan.scorboard.Team;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,11 +11,18 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 public class ScoreboardTest {
+    @DisplayName( value = "start new match" )
     @Test
     void testCreateNewMatch_whenNewMatchStart() {
         Scoreboard scoreboard = new Scoreboard();
-        scoreboard.startNewMatch( "Mexico", "Canada" );
-        assertEquals( 1, scoreboard.getMatches().size() );
+        Team home = new Team( "Mexico" );
+        Team away = new Team( "Canada" );
+        scoreboard.startNewMatch( home, away );
+        assertFalse( scoreboard.getMatches().isEmpty() );
+        assertEquals( "Mexico", scoreboard.getMatches().get( 0 ).getHomeTeam().getName() );
+        assertEquals( 0, scoreboard.getMatches().get( 0 ).getHomeTeam().getScore() );
+        assertEquals( "Canada", scoreboard.getMatches().get( 0 ).getAwayTeam().getName() );
+        assertEquals( 0, scoreboard.getMatches().get( 0 ).getAwayTeam().getScore() );
     }
 
 }

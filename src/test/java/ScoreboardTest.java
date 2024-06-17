10dfb1c883f6bf974eb2bc56001c1a0ea1582335
Rayan <Rayan.Aksu@gmail.com>
2024-06-17 20:07:49 +0300

    write testSummeryOfMatchOrderByTotalScore

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.rayan.scorboard.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -76,6 +78,7 @@ class ScoreboardTest {
         assertThrows( ScoreNotValidException.class, () -> home.setScore( -1 ) );
         assertThrows( ScoreNotValidException.class, () -> away.setScore( -1 ) );
     }
+
     @DisplayName( "finish match" )
     @Test
     void testFinishMatch() {
@@ -87,5 +90,54 @@ class ScoreboardTest {
         assertTrue( scoreboard.getMatches().isEmpty() );
     }
 
+    @DisplayName( "summary of matches in progress ordered by their total score" )
+    @Test
+    void testSummeryOfMatchOrderByTotalScore() {
+        Scoreboard scoreboard = new Scoreboard();
+        Team home1 = new Team( "Mexico" );
+        Team away1 = new Team( "Canada" );
+        scoreboard.startNewMatch( home1, away1 );
+        home1.setScore( 0 );
+        away1.setScore( 5 );
+        scoreboard.updateMatch( home1, away1 );
+
+        Team home2 = new Team( "Spain" );
+        Team away2 = new Team( "Brazil" );
+        scoreboard.startNewMatch( home2, away2 );
+        home2.setScore( 10 );
+        away2.setScore( 2 );
+        scoreboard.updateMatch( home2, away2 );
+
+        Team home3 = new Team( "Germany" );
+        Team away3 = new Team( "France" );
+        scoreboard.startNewMatch( home3, away3 );
+        home3.setScore( 2 );
+        away3.setScore( 2 );
+        scoreboard.updateMatch( home3, away3 );
+
+        Team home4 = new Team( "Uruguay" );
+        Team away4 = new Team( "Italy" );
+        scoreboard.startNewMatch( home4, away4 );
+        home4.setScore( 6 );
+        away4.setScore( 6 );
+        scoreboard.updateMatch( home4, away4 );
+
+        Team home5 = new Team( "Argentina" );
+        Team away5 = new Team( "Australia" );
+        scoreboard.startNewMatch( home5, away5 );
+        home5.setScore( 3 );
+        away5.setScore( 1 );
+        scoreboard.updateMatch( home5, away5 );
+
+        List<Match> summaryOfMatches = scoreboard.getSummaryOfMatches();
+        assertFalse( summaryOfMatches.isEmpty() );
+        assertEquals( new Match( home4, away4 ), summaryOfMatches.get( 0 ) );
+        assertEquals( new Match( home2, away2 ), summaryOfMatches.get( 1 ) );
+        assertEquals( new Match( home1, away1 ), summaryOfMatches.get( 2 ) );
+        assertEquals( new Match( home5, away5 ), summaryOfMatches.get( 3 ) );
+        assertEquals( new Match( home3, away3 ), summaryOfMatches.get( 4 ) );
+
+
+    }
 
 }
